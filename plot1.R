1340cc15b2507fdae65b091af950faa895dbcdf2
phyhouhou <phy.hyh@gmail.com>
2016-06-12 00:27:49 -0500

    update plot1.R

--- plot1.R
+++ plot1.R
@@ -14,6 +14,7 @@ df<-data.frame(subset(power_consumption,
 #convert the Date and Time variables
 datetime<-strptime(paste(as.Date(df$Date,format='%d/%m/%Y'),df$Time),
                    format="%Y-%m-%d %H:%M:%S")
+library(dplyr)
 data0702<-cbind(datetime,select(df,-(Date:Time)))
 
 #plot1
@@ -23,48 +24,3 @@ hist(data0702$Global_active_power,main='Global Active Power',
 dev.off()
 
 
-#plot2
-png(filename='plot2.png', width=480, height=480, units='px')
-with(data0702, plot(datetime,Global_active_power,xlab='',
-                    ylab='Global Active Power (kilowatts)',
-                    type='l'))
-dev.off()
-
-#plot3
-png(filename='plot3.png', width=480, height=480, units='px')
-with(data0702, plot(datetime,y=Sub_metering_1,xlab='',
-                    ylab='Energy sub metering',
-                    type='l',col='black'))
-with(data0702, lines(datetime,y=Sub_metering_2,col='red'))
-with(data0702, lines(datetime,y=Sub_metering_3,col='blue'))
-legend('topright', lty=c(1,1,1),
-       col = c('black','red','blue'),
-       legend = c('Sub_metering_1','Sub_metering_2',
-                  'Sub_metering_3'))
-dev.off()
-
-
-#plot4
-png(filename='plot4.png', width=480, height=480, units='px')
-par(mfrow=c(2,2))
-with(data0702, {
-        plot(datetime, Global_active_power,xlab='',
-             ylab='Global Active Power',type='l')
-        
-        plot(datetime, Voltage,type='l')
-        
-        
-        plot(datetime,y=Sub_metering_1,xlab='',
-             ylab='Energy sub metering',
-             type='l',col='black')
-        lines(datetime,y=Sub_metering_2,col='red')
-        lines(datetime,y=Sub_metering_3,col='blue')
-        
-        legend('topright', lty=c(1,1,1),
-               col = c('black','red','blue'),
-               legend = c('Sub_metering_1','Sub_metering_2',
-                          'Sub_metering_3'))
-        
-        plot(datetime, Global_reactive_power,type='l')
-})
-dev.off()
